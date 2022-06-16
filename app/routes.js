7e55289403d123cfa2adffdbcefabc99f294845b
Leonard Constant <leonardconstant1995@gmail.com>
2022-06-16 11:45:50 -0400

    update

--- app/routes.js
+++ app/routes.js
@@ -100,18 +100,19 @@ module.exports = function(app, passport, db, uuidV4) {
 
 // message board routes ===============================================================
 
-    app.post('/messages', (req, res) => {
-      db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/profile')
-      })
-    })
+    // app.post('/messages', (req, res) => {
+    //   db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    //     if (err) return console.log(err)
+    //     console.log('saved to database')
+    //     res.redirect('/profile')
+    //   })
+    // })
     app.post('/languagesSend', (req, res) => {
-      db.collection('languages').save({target: req.body.target, native: req.body.native}, (err, result) => {
+      db.collection('languages').save({langauge: req.body.language, yourLanguage: req.body.yourLanguage}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/main')
+        console.log(req.body.target)
       })
     })
 
