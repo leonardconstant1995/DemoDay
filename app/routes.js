b6d9ec4b14d180507d523216dbd1f639ac536942
Leonard Constant <leonardconstant1995@gmail.com>
2022-06-16 18:58:29 -0400

    update

--- app/routes.js
+++ app/routes.js
@@ -108,7 +108,7 @@ module.exports = function(app, passport, db, uuidV4) {
     //   })
     // })
     app.post('/languagesSend', (req, res) => {
-      db.collection('languages').save({langauge: req.body.language, yourLanguage: req.body.yourLanguage}, (err, result) => {
+      db.collection('languages').save({user_id: ObjectId(req.user._id),langauge: req.body.language, yourLanguage: req.body.yourLanguage}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/main')
