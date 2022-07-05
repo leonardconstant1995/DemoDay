99bad0eb526e1b32e884a4f0f7643200108836e1
Leonard Constant <leonardconstant1995@gmail.com>
2022-07-05 01:28:35 -0400

    update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const privateVapidKeys = "pqWCmaasnf131Egh4L4hNq3oujU8I03G00pLveduUD4";
 
 webPush.setVapidDetails('mailto:test@test.com', publicVapidKeys, privateVapidKeys)
 
+
 var app      = express();
 const server = require("http").Server(app)
 
