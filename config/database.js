429abb82159ffb825fba1d019c446b0a1a4a79d9
Leonard Constant <leonardconstant1995@gmail.com>
2022-07-27 13:57:07 -0400

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
+    
     'url' : 'mongodb+srv://leoconstant:leo@cluster0.ari5v8s.mongodb.net/DuoLect?retryWrites=true&w=majority', 
     'dbName': 'DuoLect'
 };
